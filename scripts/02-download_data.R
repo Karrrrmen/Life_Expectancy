e644016a55316e8cf9ec307c1f1a1bd251376072
Karrrrmen <113360429+Karrrrmen@users.noreply.github.com>
2024-11-16 18:42:44 -0500

    update download_data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,27 +1,20 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from 'https://www.kaggle.com/datasets/kumarajarshi/life-expectancy-who'
-# Author: Yanfei Huang
+# Author: Manjun Zhu
 # Date: 27 November 2024
-# Contact: Yanfei Huang
+# Contact: karmen.zhu@utoronto.ca
 # License: University of Toronto
 # Pre-requisites: No
 # Any other information needed? Thanks to Kaggle opendataset
 
 
 #### Workspace setup ####
-
 library(tidyverse)
 library(dplyr)
 
 #### Download data ####
-raw_expectancy <- read.csv(
-  file = ""
-)
-
-
+raw_data <- read.csv(file = "data/01-raw_data/LifeExpectancyData.csv")
 
 #### Save data ####
 # change raw_data to whatever name you assigned when you downloaded it.
-write_csv(raw_expectancy, "data/01-raw_data/raw_data.csv") 
-
-         
+write_csv(raw_data, "data/01-raw_data/raw_data.csv") 
