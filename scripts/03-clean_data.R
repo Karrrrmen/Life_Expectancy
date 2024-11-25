286ed51070b4b0c9eeb721a7bef19a7a3f585b4c
Karrrrmen <113360429+Karrrrmen@users.noreply.github.com>
2024-11-24 20:05:01 -0500

    Add additional predictor

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Clean the raw data recorded by WHO, removing unnecessary columns, and save it as csv and parquet.
 # Author: Manjun Zhu
-# Date: 16 November 2024
+# Date: 23 November 2024
 # Contact: karmen.zhu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: finish raw-data
@@ -13,19 +13,20 @@ library(arrow)
 #### Clean data ####
 data <- read_csv("data/01-raw_data/raw_data.csv")
 
-data <- data %>%
+analysis_data <- data %>%
   # Select the desired columns
-  select(Country, Year, Status, Life.expectancy, GDP, Diphtheria, Total.expenditure, BMI, Schooling) %>%
+  select(Country, Year, Status, Life.expectancy, GDP, Diphtheria, 
+         Total.expenditure, BMI, Schooling, pct_doctor, pct_nursing) %>%
   # Filter rows where Year is between 2009 and 2015
-  filter(Year >= 2009 & Year <= 2015) %>%
+  filter(Year >= 2004 & Year <= 2015) %>%
   # Remove rows with any missing values
   drop_na()
 
 #### Rename column
-colnames(data)[colnames(data) == "Life.expectancy"] <- "LifeExpectancy"
-colnames(data)[colnames(data) == 
+colnames(analysis_data)[colnames(analysis_data) == "Life.expectancy"] <- "LifeExpectancy"
+colnames(analysis_data)[colnames(analysis_data) == 
                  "Total.expenditure"] <- "TotalExpenditure"
 
 #### Save data ####
-write_csv(data, "data/02-analysis_data/analysis_data.csv")
-write_parquet(data, "data/02-analysis_data/analysis_data.parquet")
+write_csv(analysis_data, "data/02-analysis_data/analysis_data.csv")
+write_parquet(analysis_data, "data/02-analysis_data/analysis_data.parquet")
