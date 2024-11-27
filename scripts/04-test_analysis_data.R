a571d0c462aea2c3dead78eba5024e16b851a5bd
Karrrrmen <113360429+Karrrrmen@users.noreply.github.com>
2024-11-26 19:34:05 -0500

    Update 04-test_analysis_data.R

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Performs a series of validation tests on cleaned Life Expectancy Data
 # Author: Manjun Zhu
-# Date: 23 November 2024
+# Date: 26 November 2024
 # Contact: karmen.zhu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: The `tidyverse` , `arrow`, and `testthat` packages must be installed.
@@ -15,6 +15,7 @@ library(arrow)
 
 analysis_data <- read_parquet(here::here("data/02-analysis_data/analysis_data.parquet"))
 
+
 #### Test data ####
 # Create list for results
 test_results <- list()
@@ -23,16 +24,17 @@ test_results <- list()
 test_results$structure <- test_that("Data structure", {
   expect_true(is.data.frame(analysis_data), 
               info = "analysis_data is not a data frame.")
-  expect_equal(ncol(analysis_data), 9, info = "analysis_data does 
+  expect_equal(ncol(analysis_data), 10, info = "analysis_data does 
                not have the correct number of columns.")
-  expect_equal(nrow(analysis_data), 920, info = "analysis_data does 
+  expect_equal(nrow(analysis_data), 1021, info = "analysis_data does 
                not have the correct number of rows.")
 })
 
 # Test if column names match expected names
 test_results$column_names_test <- test_that("Column names", {
   expected_colnames <- c("Country", "Year", "Status", "LifeExpectancy", "GDP", 
-                         "Diphtheria", "Schooling", "TotalExpenditure", "BMI")
+                         "Diphtheria", "TotalExpenditure", "BMI", "Schooling", 
+                         "pct_doctor", "pct_nursing")
   expect_equal(names(analysis_data), expected_colnames, 
                info = "Column names do not match expected names.")
 })
@@ -41,9 +43,12 @@ test_results$column_names_test <- test_that("Column names", {
 test_results$na_values_test <- test_that("NA values", {
   expect_false(any(is.na(analysis_data$LifeExpectancy)), 
                info = "LifeExpectancy contains NA values.")
-  expect_false(any(is.na(analysis_data$GDP)), info = "GDP contains NA values.")
   expect_false(any(is.na(analysis_data$Diphtheria)), 
                info = "Diphtheria contains NA values.")
+  expect_false(any(is.na(analysis_data$pct_doctor)), 
+               info = "pct_doctor contains NA values.")
+  expect_false(any(is.na(analysis_data$pct_nursing)), 
+               info = "pct_nursing contains NA values.")
   expect_false(any(is.na(analysis_data$Schooling)), 
                info = "Schooling contains NA values.")
   expect_false(any(is.na(analysis_data$TotalExpenditure)), 
@@ -53,7 +58,7 @@ test_results$na_values_test <- test_that("NA values", {
 
 # Test if Year values are within expected range (2009-2015)
 test_results$year_range_test <- test_that("Year range", {
-  expect_true(all(analysis_data$Year >= 2009 & analysis_data$Year <= 2015), 
+  expect_true(all(analysis_data$Year >= 2004 & analysis_data$Year <= 2015), 
               info = "Year values are out of expected range (2009 to 2015).")
 })
 
@@ -72,12 +77,11 @@ test_results$life_expectancy_test <- test_that("LifeExpectancy range", {
               of expected range (30 to 90 years).")
 })
 
-# Test if GDP values are within expected range (e.g., 1000 to 100000 USD)
-test_results$gdp_test <- test_that("GDP range", {
-  expect_true(all(analysis_data$GDP >= 1000 & 
-                    analysis_data$GDP <= 100000, na.rm = TRUE),
-              info = "GDP values are out of expected range 
-              (1000 to 100000 USD).")
+# Test if TotalExpenditure values are within expected range (e.g., 1 to 30)
+test_results$bmi_test <- test_that("TotalExpenditure range", {
+  expect_true(all(analysis_data$TotalExpenditure >= 1 & 
+                    analysis_data$TotalExpenditure <= 30, na.rm = TRUE),
+              info = "TotalExpenditure values are out of expected range (1 to 30).")
 })
 
 # Test if Diphtheria vaccination rates are within expected range 
@@ -95,6 +99,7 @@ test_results$bmi_test <- test_that("BMI range", {
                     analysis_data$BMI <= 50, na.rm = TRUE),
               info = "BMI values are out of expected range (10 to 50).")
 })
+
 
 # Test if Schooling values are within expected range (3 to 30)
 test_results$schooling_range_test <- test_that("Schooling range", {
