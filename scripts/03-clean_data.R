10c1f6ee30ed2c5d453d33faf2a385a4f7c7847c
Karrrrmen <113360429+Karrrrmen@users.noreply.github.com>
2024-11-26 19:33:07 -0500

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Clean the raw data recorded by WHO, removing unnecessary columns, and save it as csv and parquet.
 # Author: Manjun Zhu
-# Date: 23 November 2024
+# Date: 26 November 2024
 # Contact: karmen.zhu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: finish raw-data
@@ -15,10 +15,12 @@ data <- read_csv("data/01-raw_data/raw_data.csv")
 
 analysis_data <- data %>%
   # Select the desired columns
-  select(Country, Year, Status, Life.expectancy, GDP, Diphtheria, 
+  select(Country, Year, Status, Life.expectancy, Diphtheria, 
          Total.expenditure, BMI, Schooling, pct_doctor, pct_nursing) %>%
-  # Filter rows where Year is between 2009 and 2015
+  # Filter rows where Year is between 2009 and 2015, and drop the unrealistic value
   filter(Year >= 2004 & Year <= 2015) %>%
+  filter(pct_doctor >= 0 & pct_doctor <= 1500) %>%
+  filter(pct_nursing >= 0 & pct_nursing <= 1500) %>%
   # Remove rows with any missing values
   drop_na()
 
